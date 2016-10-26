95448e8780694ed39e107a3960c4edd04adb5317
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-10-26 12:25:42 -0400

    GuiManager and Screen support

--- Game.cpp
+++ Game.cpp
@@ -1,40 +1,44 @@
 #include "Game.h"
 #include "Renderer.h"
+#include "GuiManager.h"
 
 #undef main
 
 int main()
 {
+	GuiManager gui_manager;
 
 	if(!Renderer::init())
 		return 1;
 
 	//font color
-	SDL_Color whiteColor = { 255,255,255 };
 
 
+
+	Screen* startScreen = new Screen();
+	
+	gui_manager.setScreen(startScreen);
 
 
 	//font location / bounds
 
-	SDL_Rect text_rect;
-	text_rect.x = 0;
-	text_rect.y = 0;
-	text_rect.w = 100;
-	text_rect.h = 40;
-	SDL_Texture* titleText = Renderer::CreateTexture("d&d game", Renderer::FONT_ROBOTO_24, whiteColor);
 
 	//render loop
 	while(true) //TODO: Add loop exit.
 	{
+		//logic here
+		gui_manager.runTick();
+
 		Renderer::iniRender();
+
+		gui_manager.renderScreen();
 		//render here
-		Renderer::RenderTexture(titleText, &text_rect);
+
 		Renderer::render();
 
 	}
 
-	Renderer::destroyTexture(titleText);
+	
 	Renderer::destroyRenderer();
 
 }
