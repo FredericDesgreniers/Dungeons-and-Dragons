8546c9465bb96d3e9974aa4afb56d01fff5d63ec
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-10-26 21:29:48 -0400

    Created Button

---
+++ Button.cpp
@@ -0,0 +1,31 @@
+#include "Button.h"
+#include <SDL/SDL.h>
+#include "Renderer.h"
+
+Button::Button(int id, std::string text, int x, int y,int width, int height):id(id), text(text), x(x), y(y), height(height), width(width)
+{
+	texture = Renderer::CreateTexture(text, Renderer::FONT_ROBOTO_24, { 0,0,0,255 });
+}
+
+void Button::render()
+{
+	int mx, my;
+
+	SDL_GetMouseState(&mx, &my);
+
+
+	if(isInBounds(mx,my))
+		Renderer::setColor(217,217,217,255);
+	else
+		Renderer::setColor(255,255,255,255);
+
+	Renderer::RenderRect(x, y, width, height);
+
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	rect.w = width;
+	rect.h = height;
+
+	Renderer::RenderTexture(texture, &rect);
+}
---
+++ Button.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <SDL/SDL.h>
+
+class Button
+{
+public:
+	Button(int id, std::string text, int x, int y, int width, int height);
+
+	bool isInBounds(int x, int y) { return x >= this->x && y >= this->y && x <= this->x + width && y <= this->y + height; }
+
+	void render();
+
+	int getX() { return x; }
+	int getY() { return y; }
+
+	int getWidth() { return width; }
+	int getHeight() { return height; }
+	
+	int getId() { return id; }
+
+private:
+	int id, x, y, width, height;
+	std::string text;
+
+	SDL_Texture* texture;
+};
