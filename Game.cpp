df8ed418b7bb2fe19b687ba6d2451d1efec48cf9
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-10-26 21:31:00 -0400

    Added events support and changed starting screen

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include "Renderer.h"
 #include "GuiManager.h"
+#include "ScreenMain.h"
 
 #undef main
 
@@ -15,17 +16,33 @@ int main()
 
 
 
-	Screen* startScreen = new Screen();
+	ScreenMain* startScreen = new ScreenMain();
 	
 	gui_manager.setScreen(startScreen);
 
 
 	//font location / bounds
 
+	
 
 	//render loop
 	while(true) //TODO: Add loop exit.
 	{
+		SDL_Event event;
+		while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				
+			case SDL_MOUSEBUTTONUP:
+				int x, y;
+
+				SDL_GetMouseState(&x, &y);
+				gui_manager.clickScreen(x, y);
+				break;
+			}
+		}
+
 		//logic here
 		gui_manager.runTick();
 
