d9d777e887d44fa62d236f486906eae6a9e8b136
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-10-28 22:40:23 -0400

    Removed useless code and added tick to components.

--- Screen.cpp
+++ Screen.cpp
@@ -8,13 +8,6 @@ Screen::Screen(Game* game):game(game)
 
 void Screen::render()
 {
-	SDL_Rect text_rect;
-	text_rect.x = 0;
-	text_rect.y = 0;
-	text_rect.w = 100;
-	text_rect.h = 40;
-
-	Renderer::RenderTexture(titleText, &text_rect);
 
 	for(Component* comp:components)
 	{
@@ -25,7 +18,8 @@ void Screen::render()
 
 void Screen::tick()
 {
-	
+	for (Component* comp : components)
+		comp->tick();
 }
 
 
