bb9587ed257d8d80878c19ed904b147569d7d5a6
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-01 16:15:23 -0400

    callback added

--- MapComponent.cpp
+++ MapComponent.cpp
@@ -22,10 +22,16 @@ void MapComponent::render() {
 			int y = j*tileHeight + getPositionY();
 
 			bool hover = mx > x && mx < x + tileWidth && my>y && my < y + tileHeight;
-			if (!previewMode && hover)
-				Renderer::setColor(255, 255, 255, 255);
-			else
-				Renderer::setColor(200,200,200,255);
+			
+			if(map->getTile(i,j)->getId() == TILE_WALL)
+			{	
+				Renderer::setColor(20, 20, 20, 255);
+			}else{
+				if (!previewMode && hover)
+					Renderer::setColor(255, 255, 255, 255);
+				else
+					Renderer::setColor(200,200,200,255);
+			}
 			Renderer::RenderRect(x, y, tileWidth, tileHeight);
 
 			Renderer::setColor(100, 100, 100, 255);
@@ -42,4 +48,22 @@ void MapComponent::tick() {
 
 void MapComponent::renderDebug() {
 	Style::renderDebug();
+}
+
+
+void MapComponent::click(int x, int y)
+{
+	int mapWidth = map->getWidth();
+	int mapHeight = map->getHeight();
+
+	int tileWidth = getWidth() / mapWidth;
+	int tileHeight = getHeight() / mapHeight;
+
+	int tileX = x / tileWidth;
+	int tileY = y / tileHeight;
+
+	for (int i = 0; i < onTileClick_callbacks.size(); i++)
+	{
+		onTileClick_callbacks[i](map, tileX, tileY);
+	}
 }
--- MapComponent.h
+++ MapComponent.h
@@ -11,7 +11,17 @@ public:
 	void setPreviewMode(bool previewMode) {
 		this->previewMode = previewMode;
 	}
+
+	void click(int x, int y) override;
+
+	void addOnTileClickedCallback(std::function<void(Map*, int, int)> func)
+	{
+		onTileClick_callbacks.push_back(func);
+	}
+
 private:
 	Map* map;
 	bool previewMode;
+
+	std::vector<std::function<void(Map*, int, int)>> onTileClick_callbacks;
 };
