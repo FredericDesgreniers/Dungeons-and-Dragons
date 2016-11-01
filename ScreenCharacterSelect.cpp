502454c0f86a5d1115bc75fc585d382002db3bee
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-01 18:42:24 -0400

    Small text change

--- ScreenCharacterSelect.cpp
+++ ScreenCharacterSelect.cpp
@@ -8,6 +8,6 @@ ScreenCharacterSelect::ScreenCharacterSelect(Game* game, Campaign* campaign):Scr
 void ScreenCharacterSelect::render()
 {
 	Screen::render();
-	Renderer::drawString("Choosing character to play " + campaign->getName() + " campaign with...", Renderer::FONT_ROBOTO.get(24), 0,0,1,{255,255,255,255});
+	Renderer::drawString("Chose a character to play the " + campaign->getName() + " campaign with...", Renderer::FONT_ROBOTO.get(24), 0,0,1,{255,255,255,255});
 	
 }
