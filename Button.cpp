e389e457465d3326f3bec5056c8196baed194661
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-01 18:49:50 -0400

    Changed default style

--- Button.cpp
+++ Button.cpp
@@ -6,6 +6,14 @@ Button::Button(std::string text, Font* font, int x, int y,int width, int height)
 {
 
 	texture = Renderer::CreateTexture(text, font->get(fontSize), { 0,0,0,255 });
+	setFontColor(255, 255, 255, 255);
+
+	setBackgroundColor_both(255, 255, 255, 0);
+
+	setFontUnderlineColor_hover(255, 255, 255, 255);
+	setFontUnderline_hover(1);
+
+	setPadding(10);
 
 }
 
