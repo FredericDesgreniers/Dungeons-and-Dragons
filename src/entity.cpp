f775ec7ddecb5c6597fbdf4c87cca8af19410cdc
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-02 18:33:54 -0400

    Entity has position and displayChar

---
+++ src/entity.cpp
@@ -0,0 +1,6 @@
+#include "entity.h"
+
+Entity::Entity(char renderChar):renderChar(renderChar)
+{
+	
+}
--- src/entity.h
+++ src/entity.h
@@ -2,5 +2,29 @@
 
 class Entity
 {
-	
+public:
+	Entity(char renderChar);
+
+	void setPosition(int x, int y)
+	{
+		positionX = x;
+		positionY = y;
+	}
+	int getPositionX()
+	{
+		return positionX;
+	}
+	int getPositionY()
+	{
+		return positionY;
+	}
+
+	char getRenderChar()
+	{
+		return renderChar;
+	}
+
+private:
+	int positionX, positionY;
+	char renderChar = ' ';
 };
