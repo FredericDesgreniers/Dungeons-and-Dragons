2a3d86ca647324305b6d4b72695eb94333cf587d
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-02 08:16:06 -0400

    Changed y position of character component

--- src/GUI/Screens/ScreenPlayCampaign.cpp
+++ src/GUI/Screens/ScreenPlayCampaign.cpp
@@ -7,7 +7,7 @@ ScreenPlayCampaign::ScreenPlayCampaign(Game* game, Campaign* campaign, Character
 	map_component = new MapComponent(currentmap, 0, 0, 450, 450);
 	addComponent(map_component);
 
-	character_component = new CharacterComponent(character, 500, 0, 200, 500);
+	character_component = new CharacterComponent(character, 500, 30, 200, 500);
 	character_component->setBorderSize(1);
 	character_component->setBorderColor_both(150, 150, 140, 255);
 	character_component->setPadding(10, 10, 10, 10);
