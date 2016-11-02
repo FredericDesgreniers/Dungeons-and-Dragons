6397de99275340753a74c8059bd17b5e89f26bd8
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-02 18:35:07 -0400

    Let's the character walk through the map

--- src/GUI/Screens/ScreenPlayCampaign.cpp
+++ src/GUI/Screens/ScreenPlayCampaign.cpp
@@ -12,7 +12,42 @@ ScreenPlayCampaign::ScreenPlayCampaign(Game* game, Campaign* campaign, Character
 	character_component->setBorderColor_both(150, 150, 140, 255);
 	character_component->setPadding(10, 10, 10, 10);
 	addComponent(character_component);
+
+	currentmap->spawnEntity(character, 0, 0);
+
 }
+
+void ScreenPlayCampaign::keyPressed(SDL_Keycode code)
+{
+	switch (code)
+	{
+	case(SDLK_w) : {
+		if (currentmap->moveEntity(character, character->getPositionX(), character->getPositionY() - 1) != nullptr) {
+
+		}
+		break;
+	}
+	case(SDLK_s) : {
+		if (currentmap->moveEntity(character, character->getPositionX(), character->getPositionY() + 1) != nullptr) {
+
+		}
+		break;
+	}
+	case(SDLK_a) : {
+		if (currentmap->moveEntity(character, character->getPositionX()-1, character->getPositionY()) != nullptr) {
+
+		}
+		break;
+	}
+	case(SDLK_d) : {
+		if (currentmap->moveEntity(character, character->getPositionX()+1, character->getPositionY()) != nullptr) {
+
+		}
+		break;
+	}
+	}
+}
+
 
 void ScreenPlayCampaign::render()
 {
