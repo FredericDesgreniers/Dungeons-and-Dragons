7c23dda5ec6c9bbb860ce50d12540353c6d719f3
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-01 20:30:13 -0400

    Button is now just a label child that sets the default style

--- Button.cpp
+++ Button.cpp
@@ -2,10 +2,9 @@
 #include <SDL/SDL.h>
 #include "Renderer.h"
 
-Button::Button(std::string text, Font* font, int x, int y,int width, int height):Style(x, y, width, height), text(text), font(font)
+Button::Button(std::string text, Font* font, int x, int y,int width, int height):Label(text,font, x, y, width, height)
 {
 
-	texture = Renderer::CreateTexture(text, font->get(fontSize), { 0,0,0,255 });
 	setFontColor(255, 255, 255, 255);
 
 	setBackgroundColor_both(255, 255, 255, 0);
@@ -17,49 +16,4 @@ Button::Button(std::string text, Font* font, int x, int y,int width, int height)
 
 }
 
-void Button::render()
-{
-	int mx, my;
 
-	SDL_GetMouseState(&mx, &my);
-	bool hover = isInBounds(mx, my);
-
-	drawDefaultStyle(hover);
-
-	Renderer::RenderTexture(texture, &dimensions);
-
-	if(hover && fontUnderline_hover > 0)
-	{
-		Renderer::setColor(fontUnderlineColor_hover);
-		Renderer::drawLine(dimensions.x, dimensions.y + dimensions.h, dimensions.x + dimensions.w, dimensions.y + dimensions.h);
-
-	}else if (fontUnderline > 0)
-	{
-		Renderer::setColor(fontUnderlineColor);
-		Renderer::drawLine(dimensions.x, dimensions.y + dimensions.h, dimensions.x + dimensions.w, dimensions.y + dimensions.h);
-	}
-}
-
-void Button::setFontColor(int r, int g, int b, int a)
-{
-	Style::setFontColor(r, g, b, a);
-	reloadTexture();
-}
-
-void Button::setFontSize(int size)
-{
-	Style::setFontSize(size);
-	reloadTexture();
-}
-
-void Button::reloadTexture()
-{
-	Renderer::destroyTexture(texture);
-	texture = Renderer::CreateTexture(text, font->get(fontSize), fontColor);
-}
-
-
-Button::~Button()
-{
-	Renderer::destroyTexture(texture);
-}
--- Button.h
+++ Button.h
@@ -4,40 +4,19 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
 #include "Font.h"
+#include "Label.h"
 
-class Button : public Style
+class Button : public Label
 {
 public:
 	Button(std::string text,Font* font, int x, int y, int width, int height);
 
-
-
-	/**
-	 * Render button on screen
-	 */
-	void render();
-
-
-	/**
-	 * Set the font color for the button text. 
-	 * Calling this will destroy and create a texture.
-	 * Do not call this often
-	 */
-	void setFontColor(int r, int g, int b, int a) override;
-
-	void setFontSize(int size) override;
-
-	void reloadTexture();
-
 	void adjustButtonDimensions()
 	{
-		TTF_SizeText(font->get(fontSize), text.c_str(),  &dimensions.w, &dimensions.h);
+		Label::adjustDimensions();
 	}
 
-	~Button();
 
-private:
-	std::string text;
-	Font* font;
-	SDL_Texture* texture;
+
+
 };
