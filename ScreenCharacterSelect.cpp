86c99e1482a92049616ead0d7a20d794f48d743e
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-01 20:02:58 -0400

    Added a default random character button and made it go to the play screen

--- ScreenCharacterSelect.cpp
+++ ScreenCharacterSelect.cpp
@@ -1,8 +1,18 @@
 #include "ScreenCharacterSelect.h"
 #include "Renderer.h"
+#include "ScreenPlayCampaign.h"
+
 ScreenCharacterSelect::ScreenCharacterSelect(Game* game, Campaign* campaign):Screen(game), campaign(campaign)
 {
-	
+	Button* defaultCharacterBtn = new Button("Random character roll", &Renderer::FONT_ROBOTO, 10,300,0,0);
+	defaultCharacterBtn->adjustButtonDimensions();
+	defaultCharacterBtn->addOnClick_callback([this](Component* component, int x, int y)
+	{
+		ScreenPlayCampaign* screen_play_campaign = new ScreenPlayCampaign(this->game, this->campaign, new Character(1));
+		Renderer::addVoidScreen(this->game->getGuiManager()->setScreen(screen_play_campaign));
+	});
+
+	addComponent(defaultCharacterBtn);
 }
 
 void ScreenCharacterSelect::render()
