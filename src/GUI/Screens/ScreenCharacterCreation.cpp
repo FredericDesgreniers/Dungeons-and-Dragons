55eb018450496051d0d01b1eea7d07db31571676
lotxi <tim.gotts@gmail.com>
2016-11-03 17:23:18 -0400

    Added character instantiation on confirm

--- src/GUI/Screens/ScreenCharacterCreation.cpp
+++ src/GUI/Screens/ScreenCharacterCreation.cpp
@@ -1,14 +1,10 @@
 #include "ScreenCharacterCreation.h"
-#include "screenMain.h"
-#include "../Components/TextField.h"
-#include "../Renderer.h"
-#include "../../dice.h"
-#include <string>
+
 
 ScreenCharacterCreation::ScreenCharacterCreation(Game* game) : Screen(game)
 {
 	strength = "     -", constitution = "     -", dexterity = "     -", intelligence = "     -", wisdom = "     -", charisma = "     -";
-	TextField* nameInput = new TextField("Test", 250, 100, 100, 20);
+	nameInput = new TextField("Test", 250, 100, 100, 20);
 	nameInput->setFontSize(15);
 
 	Button* rollBtn = new Button("Roll!", &Renderer::FONT_ROBOTO, 400, 300, 100, 30);
@@ -20,7 +16,12 @@ ScreenCharacterCreation::ScreenCharacterCreation(Game* game) : Screen(game)
 	Button* confirmBtn = new Button("Confirm", &Renderer::FONT_ROBOTO, 300, 600, 100, 30);
 	rollBtn->adjustButtonDimensions();
 
-
+	addComponent((new Label("Strength", &Renderer::FONT_ROBOTO, 100, 175, 1, 1))->setFontSize(15)->adjustDimensions());
+	addComponent((new Label("Dexterity", &Renderer::FONT_ROBOTO, 100, 200, 1, 1))->setFontSize(15)->adjustDimensions());
+	addComponent((new Label("Constitution", &Renderer::FONT_ROBOTO, 100, 225, 1, 1))->setFontSize(15)->adjustDimensions());
+	addComponent((new Label("Intelligence", &Renderer::FONT_ROBOTO, 100, 250, 1, 1))->setFontSize(15)->adjustDimensions());
+	addComponent((new Label("Wisdom", &Renderer::FONT_ROBOTO, 100, 275, 1, 1))->setFontSize(15)->adjustDimensions());
+	addComponent((new Label("Charisma", &Renderer::FONT_ROBOTO, 100, 300, 1, 1))->setFontSize(15)->adjustDimensions());
 	
 
 	backBtn->addOnClick_callback([this](Component* comp, int x, int y)
@@ -39,6 +40,18 @@ ScreenCharacterCreation::ScreenCharacterCreation(Game* game) : Screen(game)
 		setWisdom(rollStat());
 		setCharisma(rollStat());
 		std::cout << "Rolling stats!" << std::endl;
+	});
+
+	confirmBtn->addOnClick_callback([this](Component* comp, int x, int y)
+	{	
+		if (!(strength== "     -")){
+		std::cout << "Character Instantiated:" << std::endl;
+		createCharacter();
+		}
+		else {
+			std::cout << "Cannot instantiate character: press roll first" << std::endl;
+
+		}
 	});
 
 
@@ -62,43 +75,25 @@ void ScreenCharacterCreation::render()
 	Renderer::drawString("Class", Renderer::FONT_ROBOTO.get(16), 100, 150, 1, { 255,255,255,255 });
 	Renderer::drawString("Warrior", Renderer::FONT_ROBOTO.get(16), 250, 150, 1, { 255,255,255,255 });
 
-	Renderer::drawString("Strength", Renderer::FONT_ROBOTO.get(16), 100, 175, 1, { 255,255,255,255 });
 	Renderer::drawString(strength, Renderer::FONT_ROBOTO.get(16), 250, 175, 1, { 255,255,255,255 });
 
-	Renderer::drawString("Constitution", Renderer::FONT_ROBOTO.get(16), 100, 200, 1, { 255,255,255,255 });
-	Renderer::drawString(constitution, Renderer::FONT_ROBOTO.get(16), 250, 200, 1, { 255,255,255,255 });
+	Renderer::drawString(dexterity, Renderer::FONT_ROBOTO.get(16), 250, 200, 1, { 255,255,255,255 });
 
-	Renderer::drawString("Dexterity", Renderer::FONT_ROBOTO.get(16), 100, 225, 1, { 255,255,255,255 });
-	Renderer::drawString(dexterity, Renderer::FONT_ROBOTO.get(16), 250, 225, 1, { 255,255,255,255 });
+	Renderer::drawString(constitution, Renderer::FONT_ROBOTO.get(16), 250, 225, 1, { 255,255,255,255 });
 
-	Renderer::drawString("Intelligence", Renderer::FONT_ROBOTO.get(16), 100, 250, 1, { 255,255,255,255 });
 	Renderer::drawString(intelligence, Renderer::FONT_ROBOTO.get(16), 250, 250, 1, { 255,255,255,255 });
 
-	Renderer::drawString("Wisdom", Renderer::FONT_ROBOTO.get(16), 100, 275, 1, { 255,255,255,255 });
 	Renderer::drawString(wisdom, Renderer::FONT_ROBOTO.get(16), 250, 275, 1, { 255,255,255,255 });
 
-	Renderer::drawString("Charisma", Renderer::FONT_ROBOTO.get(16), 100, 300, 1, { 255,255,255,255 });
 	Renderer::drawString(charisma, Renderer::FONT_ROBOTO.get(16), 250, 300, 1, { 255,255,255,255 });
 
 	Screen::render();
 }
 
 string ScreenCharacterCreation::rollStat() {
-	int rolls[4] = { 0,0,0,0 };
-	int sum = 0;
-	sum += rolls[0] = Dice::roll("1d6");
-	sum += rolls[1] = Dice::roll("1d6");
-	sum += rolls[2] = Dice::roll("1d6");
-	sum += rolls[3] = Dice::roll("1d6");
-	int smallest = rolls[0];
-	for (int i = 1; i < 4; i++) {
-		if (rolls[i] < smallest)
-			smallest = rolls[i];
-	}
-	sum -= smallest;
-
+	int roll = Dice::rollStat();
 	ostringstream StrStream;
-	StrStream << sum;
+	StrStream << roll;
 	return StrStream.str();
 }
 
@@ -124,5 +119,17 @@ void ScreenCharacterCreation::setWisdom(std::string value) {
 
 void ScreenCharacterCreation::setCharisma(std::string value) {
 	charisma = value;
+}
+
+void ScreenCharacterCreation::createCharacter() {
+	character = new Character(stoi(strength), stoi(dexterity), stoi(constitution), stoi(intelligence), stoi(wisdom), stoi(charisma));
+	std::string name = nameInput->getText();
+
+	std::cout << character->toString();
+	//TODO: SAVE CHARACTER TO FILE HERE
+
+	delete character;
+	
+	
 }
 
--- src/GUI/Screens/ScreenCharacterCreation.h
+++ src/GUI/Screens/ScreenCharacterCreation.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "Screen.h"
+#include "screenMain.h"
+#include "../../character.h"
 #include <sstream>
+#include "../Components/TextField.h"
+#include "../Renderer.h"
+#include "../../dice.h"
+#include <string>
 
 class ScreenCharacterCreation : public Screen
 {
@@ -8,17 +14,19 @@ class ScreenCharacterCreation : public Screen
 private:
 	std::string strength, constitution, dexterity, intelligence, wisdom, charisma;
 	std::string rollStat();
+	Character* character;
+	TextField* nameInput;
 
 public:
 	ScreenCharacterCreation(Game* game);
 	void render() override;
+	void createCharacter();
 	void setStrength(std::string value);
 	void setConstitution(std::string value);
 	void setDexterity(std::string value);
 	void setIntelligence(std::string value);
 	void setWisdom(std::string value);
 	void setCharisma(std::string value);
-
 
 
 
