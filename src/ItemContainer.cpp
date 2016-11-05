85b2be0a23dd854a72672e2b826c378b8c569f0d
Palmirouze <Palmirouze@gmail.com>
2016-11-05 11:49:35 -0700

    Added member variables and constructors
    Restricted bool member variable is an attempt at restriction of the type
    of Items that can be in the container.

--- src/ItemContainer.cpp
+++ src/ItemContainer.cpp
@@ -1,1 +1,35 @@
 #include "ItemContainer.h"
+
+//! Constuctor
+ItemContainer::ItemContainer()
+{
+	this->name = "Default Container";
+	this->restricted = false;
+}
+
+ItemContainer::ItemContainer(std::string name, bool restriction)
+{
+	this->name = name;
+	this->restricted = restriction;
+}
+
+//! Destructor
+ItemContainer::~ItemContainer()
+{
+
+}
+
+Item ItemContainer::getItemAtIndex(int i)
+{
+	return container[i];
+}
+
+std::string ItemContainer::getName()
+{
+	return this->name;
+}
+
+void ItemContainer::setName(std::string s)
+{
+	this->name = s;
+}
