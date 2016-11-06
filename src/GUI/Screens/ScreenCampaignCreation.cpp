492f5b4cafcb30006ebc99f6a9c2dba627f2eee2
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-06 13:08:29 -0500

    Added style to map select component

--- src/GUI/Screens/ScreenCampaignCreation.cpp
+++ src/GUI/Screens/ScreenCampaignCreation.cpp
@@ -7,9 +7,13 @@ ScreenCampaignCreation::ScreenCampaignCreation(Game* game):Screen(game)
 {
 	int wx, wy;
 	SDL_GetWindowSize(Renderer::window, &wx, &wy);
-	MapSelect* ms = new MapSelect(50,60,wx/2,wy-100);
+	MapSelect* ms = new MapSelect(50,50,wx/2-100,wy-100);
 	addComponent(ms);
-
-	TextField* nameField = new TextField("Campaign1", 10,10,300,40);
+	ms->setBackgroundColor_both(50, 50, 50, 255);
+	ms->setPadding(10, 10, 10, 10);
+	ms->setBorderColor_both(150, 150, 150, 255);
+	ms->setBorderSize(1);
+	TextField* nameField = new TextField("Campaign1", wx / 2, 5, wx / 2 - 50, 30); 
+		nameField->setFontSize(20);
 	addComponent(nameField);
 }
