b229d95a92dc6280c0243cbe7519fdf85775fe5b
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-06 12:41:14 -0500

    Added name textfield

--- src/GUI/Screens/ScreenCampaignCreation.cpp
+++ src/GUI/Screens/ScreenCampaignCreation.cpp
@@ -1,10 +1,15 @@
 #include "ScreenCampaignCreation.h"
 #include "../Components/MapSelect.h"
 #include "../Renderer.h"
+#include "../Components/TextField.h"
+
 ScreenCampaignCreation::ScreenCampaignCreation(Game* game):Screen(game)
 {
 	int wx, wy;
 	SDL_GetWindowSize(Renderer::window, &wx, &wy);
-	MapSelect* ms = new MapSelect(50,50,wx/2,wy-100);
+	MapSelect* ms = new MapSelect(50,60,wx/2,wy-100);
 	addComponent(ms);
+
+	TextField* nameField = new TextField("Campaign1", 10,10,300,40);
+	addComponent(nameField);
 }
