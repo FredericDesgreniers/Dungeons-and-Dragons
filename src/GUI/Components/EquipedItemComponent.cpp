1b9a9ca1c606e249cb8f3ad0af23a4c1cce53fa3
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-08 16:52:54 -0500

    Changed the wya items are displayed, now uses observer

--- src/GUI/Components/EquipedItemComponent.cpp
+++ src/GUI/Components/EquipedItemComponent.cpp
@@ -7,24 +7,47 @@ EquipedItemComponent::EquipedItemComponent(Item** items, int x, int y, int width
 	setFontSize(15);
 	for (int i = Item::ItemType::HELMET; i != Item::ItemType::WEAPON; i++)
 	{
-
+		
 		addComponent((new Label(Item::displayType(static_cast<Item::ItemType>(i)), &Renderer::FONT_ROBOTO, 0, i*(fontSize+20), 0, 0))->setFontSize(fontSize)->adjustDimensions());
+		Button* itemButton = (new Button(items[i] == nullptr ? "None" : items[i]->getName(), &Renderer::FONT_ROBOTO, 50, i*(fontSize+20), 0, 0));
+		itemButton->setFontSize(fontSize);
+		itemButton->adjustDimensions();
+		itemButtons.push_back(itemButton);
+		addComponent(itemButton);
+		itemButton->addOnClick_callback([this,i](Component* c, int,int)
+		{
+			for(std::function<void(Item* item)> func: this->onItemClickCallback)
+			{
+				func(this->items[i]);
+			}
+		});
+
 	}
 	this->setHeight(Item::ItemType::WEAPON * (fontSize+20) + 30);
-	this->setWidth(100);
+
 }
 
 void EquipedItemComponent::render()
 {
 	Pane::render();
 	drawDefaultStyle();
-	for (int i = Item::ItemType::HELMET; i != Item::ItemType::WEAPON; i++)
-	{
-		if(items[i]!=nullptr)
-			Renderer::drawString(items[i]->getName(), Renderer::FONT_ROBOTO.get(15), getPositionX()+60, getPositionY()+i*(fontSize+20),1,{255,255,255,255});
-		else
-			Renderer::drawString("None", Renderer::FONT_ROBOTO.get(15), getPositionX() + 60, getPositionY() + i*(fontSize + 20), 1, { 255,255,255,255 });
-	}
-
 }
 
+void EquipedItemComponent::addOnItemClick_callback(std::function<void(Item*)> func)
+{
+	onItemClickCallback.push_back(func);
+}
+
+void EquipedItemComponent::click(int x, int y)
+{
+	
+}
+
+void EquipedItemComponent::Update()
+{
+	for (int i = Item::ItemType::HELMET; i != Item::ItemType::WEAPON; i++)
+	{
+		itemButtons[i]->setText(items[i] == nullptr ? "None" : items[i]->getName());
+		itemButtons[i]->adjustDimensions();
+	}
+}
--- src/GUI/Components/EquipedItemComponent.h
+++ src/GUI/Components/EquipedItemComponent.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "Pane.h"
 #include "../../Item.h"
+#include "Button.h"
 
-class EquipedItemComponent:public Pane
+class EquipedItemComponent:public Pane, public Observer
 {
 public:
 	EquipedItemComponent(Item** items,int x, int y, int width, int height);
 	void render() override;
+	void click(int x, int y) override;
+	void addOnItemClick_callback(std::function<void(Item*)>);
+
+	void Update() override;
 private:
 	Item** items;
+	std::vector<Button*> itemButtons;
+
+	std::vector<std::function<void(Item*)>> onItemClickCallback;
 };
