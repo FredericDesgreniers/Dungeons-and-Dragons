55dd0bed402c9b491fef21f05b8324dbc31d0785
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-12-01 08:34:24 -0500

    removed wizrd simulation

--- src/Wizard..cpp
+++ src/Wizard..cpp
@@ -8,26 +8,7 @@ Wizard::Wizard():Monster('W')
 	setName("Wizard");
 }
 
-void Wizard::simulate(Map* map, Pathfinder* pathfinder)
-{
-	Character* c = map->getCharacter();
-	if (realDistanceTo(c->getPositionX(), c->getPositionY()) < 5)
-	{
-		spellProgress+=34;
-		if(spellProgress > 100)
-		{
-			spellProgress = 0;
-			if(c->hit(20))
-			{
-				//map->removeEntity(c->getPositionX(), c->getPositionY());
-			}
-		}
-	}else
-	{
-		spellProgress = 0;
-		Monster::simulate(map, pathfinder);
-	}
-}
+
 SDL_Color Wizard::getDisplayColor()
 {
 	return{ 0,0,255,255 };
