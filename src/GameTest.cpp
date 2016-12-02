f32bc41451fff1af7c140ded5a46beca574ac1e8
Palmirouze <Palmirouze@gmail.com>
2016-12-01 22:27:01 -0500

    Added test cases for character

--- src/GameTest.cpp
+++ src/GameTest.cpp
@@ -1,6 +1,6 @@
 //! @file
 //! @brief CPPUNIT test cases
-//! Two other test cases are implemented here.
+//! Game related test cases
 
 #include <cppunit/TestCase.h>
 #include <cppunit/TestFixture.h>
@@ -15,6 +15,7 @@
 #include <cppunit/XmlOutputter.h>
 #include "dice.h"
 #include "Item.h"
+#include "LivingEntity.h"
 using namespace CppUnit;
 
 //! Test Class for the Dice class
@@ -27,6 +28,8 @@ class GameTest : public CppUnit::TestFixture
 	CPPUNIT_TEST(testDiceModifier); //!test the modifier
 	CPPUNIT_TEST(testMultipleDiceRolls); //!test multiple dice rolls
 	CPPUNIT_TEST(testItemValidity);
+	CPPUNIT_TEST(testCharacterValidity);
+	CPPUNIT_TEST(testHit);
 	CPPUNIT_TEST_SUITE_END();
 protected:
 	void testDiceRollInputValidity();
@@ -34,6 +37,8 @@ protected:
 	void testDiceModifier();
 	void testMultipleDiceRolls();
 	void testItemValidity();
+	void testCharacterValidity();
+	void testHit();
 };
 
 //! cppunit test cases registration
@@ -174,6 +179,38 @@ void GameTest::testItemValidity()
 
 		delete it;
 	}
+}
+
+//! Test method for the validity of a Character
+//! Test case: checks for bounds of values for multiple characters
+void GameTest::testCharacterValidity()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		LivingEntity* c = new LivingEntity(3);
+
+		CPPUNIT_ASSERT(c->getCharisma() >= 0 && c->getCharisma() <= 18);
+		CPPUNIT_ASSERT(c->getStrength() >= 0 && c->getStrength() <= 18);
+		CPPUNIT_ASSERT(c->getDexterity() >= 0 && c->getDexterity() <= 18);
+		CPPUNIT_ASSERT(c->getIntelligence() >= 0 && c->getIntelligence() <= 18);
+		CPPUNIT_ASSERT(c->getWisdom() >= 0 && c->getWisdom() <= 18);
+		CPPUNIT_ASSERT(c->getConstitution() >= 0 && c->getConstitution() <= 18);
+		CPPUNIT_ASSERT(c->getLevel() == 3);
+
+		delete c;
+	}
+}
+
+
+//! Test method for testing the hit functionnality
+//! Test case: checks that the hitpoints get updated correctly
+void GameTest::testHit()
+{
+	LivingEntity* c = new LivingEntity(1);
+
+	c->hit(4);
+
+	CPPUNIT_ASSERT(c->getHealth() == 6);
 }
 
 
